df7ec7e6c1581f55736f63bf183b42b8b952e2e8
JackYap <junjieyjj@163.com>
2021-06-08 16:51:18 +0800

    init

--- devops_setup/infra/run.sh
+++ devops_setup/infra/run.sh
@@ -9,9 +9,11 @@ PROJECT_BASEDIR=$(dirname "${SCRIPT_BASEDIR}")
 source ${PROJECT_BASEDIR}/lib/*
 
 # include config
-[ -z $(ps -p $PPID o cmd | grep install.sh | wc -l) ] && { [ -f "${SCRIPT_BASEDIR}/config" ] && { source ${SCRIPT_BASEDIR}/config; } || { echo_red "${SCRIPT_BASEDIR}/config not exist" ; exit -1; }; } || { [ -f "${PROJECT_BASEDIR}/config" ] && { source ${PROJECT_BASEDIR}/config; } || { echo_red "${PROJECT_BASEDIR}/config not exist" ; exit -1; }; }
-
-
+if [ -z $(ps -p $PPID o cmd | grep install.sh | wc -l) ];then
+  [ -f "${SCRIPT_BASEDIR}/config" ] && { source ${SCRIPT_BASEDIR}/config; } || { echo_red "${SCRIPT_BASEDIR}/config not exist" ; exit -1; }
+else
+  [ -f "${PROJECT_BASEDIR}/config" ] && { source ${PROJECT_BASEDIR}/config; } || { echo_red "${PROJECT_BASEDIR}/config not exist" ; exit -1; }
+fi
 
 
 
