8cda498d644e3735f8432003a327d31404260e91
JackYap <junjieyjj@163.com>
2021-06-18 14:51:22 +0800

    init

--- devops_setup/install.sh
+++ devops_setup/install.sh
@@ -88,7 +88,7 @@ deploy_ingress(){
 }
 
 list_resources(){
-    echo -e "\033[1;36m1. Helm Release:\033[0m"
+    echo -e "\033[1;36m1. Helm Release\033[0m"
     echo "NAME    NAMESPACE       REVISION        UPDATED                                 STATUS          CHART             APP VERSION"
     helm -n ${namespace} list | grep -E 'gitlab|sonarqube|jenkins'
     echo
@@ -100,16 +100,57 @@ list_resources(){
     echo 
     echo -e "\033[1;36m5. PV, PVC resources\033[0m"
     echo 'NAME               CAPACITY   ACCESS MODES   RECLAIM POLICY   STATUS   CLAIM                             STORAGECLASS   REASON   AGE'
-    kubectl -n ${namespace} get pv | grep -E 'gitlab-pv|jenkins-pv|sonarqube-pv|jenkins-slave-pv'
+    kubectl get pv | grep -E 'gitlab-pv|jenkins-pv|sonarqube-pv|jenkins-slave-pv'
     echo
     echo 'NAME            STATUS   VOLUME         CAPACITY   ACCESS MODES   STORAGECLASS   AGE'
-    kubectl -n ${namespace} get pvc | grep -E 'gitlab-pv|jenkins-pv|sonarqube-pv|jenkins-slave-pv'
+    kubectl -n ${namespace} get pvc | grep -E 'gitlab-pvc|jenkins-pvc|sonarqube-pvc|jenkins-slave-pvc'
     echo
     
 }
 
 delete_resources(){
-    echo ''
+    echo -e "\033[1;36m1. Delete helm Release:\033[0m"
+    resources_count=$(helm -n ${namespace} list | grep -E 'gitlab|sonarqube|jenkins' | wc -l)
+    if [ ${resources_count} -gt 0 ];then
+        helm -n ${namespace} list | grep -E 'gitlab|sonarqube|jenkins' | awk '{print $1}' | while read line
+        do
+            helm -n ${namespace} uninstall $line
+        done
+    else
+        echo 'no releases'
+    fi
+    echo
+    echo -e "\033[1;36m2. delete shared Volume /jenkins /sonarqube /gitlab /jenkins-slave\033[0m"
+    kubectl -n ${namespace} exec -it busybox sh -- rm -fr /data/jenkins /data/sonarqube /data/gitlab 
+    echo
+    echo -e "\033[1;36m3. delete all tables in gitlab postgresql database ${gitlab_postgresql_db_database}\033[0m"
+    echo "delete gitlab postgresql"
+    echo
+    echo -e "\033[1;36m4. delete all tables in sonarqube postgresql database ${sonarqube_postgresql_db_database}\033[0m"
+    echo "delete sonarqube postgresql"
+    echo
+    echo -e "\033[1;36m5. delete PV, PVC resources\033[0m"
+    pv_count=$(kubectl -n ${namespace} get pv | grep -E 'gitlab-pv|jenkins-pv|sonarqube-pv|jenkins-slave-pv' | wc -l)
+    if [ ${pv_count} -gt 0 ];then
+        kubectl -n ${namespace} get pv | grep -E 'gitlab-pv|jenkins-pv|sonarqube-pv|jenkins-slave-pv' | awk '{print $1}' | while read line
+        do
+            kubectl delete pv $line
+        done
+    else
+        echo 'no pv'
+    fi
+
+    pvc_count=$(kubectl -n ${namespace} get pvc | grep -E 'gitlab-pv|jenkins-pv|sonarqube-pv|jenkins-slave-pv' | wc -l)
+    if [ ${pvc_count} -gt 0 ];then
+        kubectl -n ${namespace} get pvc | grep -E 'gitlab-pvc|jenkins-pvc|sonarqube-pvc|jenkins-slave-pvc' | awk '{print $1}' | while read line
+        do
+            kubectl -n ${namespace} delete pvc $line
+        done
+    else
+        echo 'no pvc'
+    fi
+    
+    echo
 }
 
 echo 'Easy DevOps
@@ -143,7 +184,12 @@ case "${choice}" in
     "6")
         echo -e "\033[31m\033[05mWARNING: THIS OPERATION WILL DELETE THE FOLLOWING RESOURCE!!! \033[0m"
         list_resources
-        echo -e "\033[31m\033[05mConfirm [Y/N] \033[0m"
+        echo -n -e "\033[31m\033[05mConfirm [Y/N] \033[0m"
+        read delete_confirm_first
+        [ "Y" == ${delete_confirm_first} ] || { exit 110; }
+        echo -n -e "\033[31m\033[05mThis operation will delete all resources, including database tables, persistent volume data, please be sure to confirm again [Y/N] \033[0m"
+        read delete_confirm_second
+        [ "Y" == ${delete_confirm_second} ] && { delete_resources; } || { exit 110; }
         ;;
     *)
         echo "unknow choice" 
